44660568126b3e0dee1da18114c9e6885d81bc55
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-08-11 16:13:09 +0600

    tailwind config added

--- phone.js
+++ phone.js
@@ -2,5 +2,8 @@
 const loadPhone = async () =>{
      const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
      const data = await res.json()
-     console.log(data)
+     const phones = data.data
+     console.log(phones)
 }
+
+loadPhone()
